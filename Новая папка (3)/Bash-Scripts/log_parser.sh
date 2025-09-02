dab2e2c0d914e9987289cb358f78a642df973dca
GooseGeneral <sult2600@gmail.com>
2025-09-02 15:36:30 +0500

    Delete Новая папка (3) directory

--- Новая папка (3)/Bash-Scripts/log_parser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-grep "ERROR" app.log > errors.txt
-echo "Найдено $(wc -l < errors.txt) ошибок"
--- Новая папка (3)/SQL/queries_examples.sql
+++
@@ -1,3 +0,0 @@
--- Примеры SQL запросов
-SELECT * FROM users;
-SELECT name, age FROM users WHERE age > 18;
